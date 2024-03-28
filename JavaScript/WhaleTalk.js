acf782af45262474a9822ef9620579876ca048d2
Tummaz00 <45412626+Tummaz00@users.noreply.github.com>
2024-03-28 09:28:40 -0400

    Update WhaleTalk.js
    Translate English to Whale language exercise

--- JavaScript/WhaleTalk.js
+++ JavaScript/WhaleTalk.js
@@ -1,1 +1,25 @@
+var input = "turpentine and turtles"
+const vowels = ['a', 'e', 'i', 'o', 'u']
+var resultArray = []
 
+for(var i = 0; i < input.length; i++){
+  //doubles e's in whale language
+   if(input[i] === 'e'){
+      resultArray.push(input[i])
+      resultArray.push(input[i])
+  }
+  //doubles u's in whale language
+  else if(input[i] === 'u'){
+        resultArray.push(input[i])
+        resultArray.push(input[i])
+    }
+  for(var j = 0; j < vowels.length; j++){
+     if(input[i] === vowels[j] && input[i] != 'e' && input[i] != 'u'){
+      resultArray.push(input[i])
+    }
+  }
+}
+
+//combines the items of array into one string (add a parameter seperator or the method will default to a comma seperator)
+var resultString = resultArray.join("").toUpperCase()
+console.log(resultString)
